9aed801bb9ed2015d3eba858e0e550c6323d6bbf
hinh <93114852+Turkey03@users.noreply.github.com>
2023-07-11 18:19:01 +0700

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const urll = 'https://hinhblog-server.onrender.com/posts';
+const urll = 'http://localhost:5000/posts';
 export const fetchPosts = () =>  axios.get(urll);
 
 export const createPost = (newPost) => axios.post(urll, newPost);
