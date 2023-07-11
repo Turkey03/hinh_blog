1acb3a57a2c7cf0162172ebc441588c606331c7e
hinh <93114852+Turkey03@users.noreply.github.com>
2023-07-11 18:11:01 +0700

    Update index.js
    update url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000/posts';
-export const fetchPosts = () =>  axios.get(URL);
+const urll = 'http://localhost:5000/posts';
+export const fetchPosts = () =>  axios.get(urll);
 
-export const createPost = (newPost) => axios.post(URL, newPost);
+export const createPost = (newPost) => axios.post(urll, newPost);
 
-export const updatePost = (id, updatedPost) => axios.patch(`${URL}/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${urll}/${id}`, updatedPost);
 
-export const likePost = (id) => axios.patch(`${URL}/${id}/likePost`);
+export const likePost = (id) => axios.patch(`$urll}/${id}/likePost`);
 
-export const deletePost = (id) => axios.delete(`${URL}/${id}`);
+export const deletePost = (id) => axios.delete(`${urll}/${id}`);
