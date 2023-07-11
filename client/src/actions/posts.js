ebd499bdfe38b3851d8b52ab2d5a1ad5e85b5c74
hinh <93114852+Turkey03@users.noreply.github.com>
2023-07-11 18:11:42 +0700

    Update posts.js
    changle console

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,7 @@
             const {data} = await api.fetchPosts();
             dispatch({type: FETCH_ALL, payload: data})
         } catch (error) {
-            console.log(error.message);
+            console.log(error);
         }
     }
     export const createPost = (post) => async (dispatch) => {
